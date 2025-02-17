64e2e3cfbba91a68fba0fff9a760bee22547b084
sheetal <sheetal.sonsale@artivatic.ai>
2025-02-17 11:27:41 +0530

    remove customer model

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -4,9 +4,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('user.urls')),
-    # path('products/', include('product.urls')),
-    # path('card/', include('card.urls')),
-    # path('orders/', include('order.urls')),
+
     
 ]
--- marketplace/user/serializers.py
+++ marketplace/user/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework import serializers
-from .models import customer
 
-class CustomerModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = customer
-        fields = ['id', 'name', 'email', 'gender','username','password']  # Fields to include in API
+
+
 
 
 
